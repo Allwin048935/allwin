a9709f87f9794244bd6d8b35b4fcc06eddca47f5
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-02-21 23:27:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,4 +47,4 @@ symbols = [
 ]
 
 # Time interval for fetching historical data
-time_interval = '1m'  # You can change this to '4h', '1d', etc.
+time_interval = '1h'  # You can change this to '4h', '1d', etc.
