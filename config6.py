77a3c5117b9ad93c167530e3978b8cf0bc137ff6
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-04-10 12:37:39 +0530

    Update config6.py

--- config6.py
+++ config6.py
@@ -1,1 +1,20 @@
 
+# config.py
+
+BINANCE_API_KEY = 'veiqd07BuRmMlxy3eeRLLKNyDnFrIphTcqgSM7XTRUCzQWTyqxK4sPtfVZioaVHi'
+BINANCE_API_SECRET = 'N4myBDNkkD203gxxpo8NLQgdZxdvzm7N7PpBjBU0DiTtctTLaFQEVev51N4P5R5g'
+
+# Telegram API keys
+#TELEGRAM_BOT_TOKEN = ''
+#TELEGRAM_CHAT_ID = ''
+
+
+# List of futures trading pairs (symbols)
+symbols = [
+    'NEARUSDT', 'NEOUSDT', 'NFPUSDT',
+    'NKNUSDT', 'NMRUSDT', 'NTRNUSDT', 'OCEANUSDT', 'OGNUSDT', 'OMGUSDT', 'OMUSDT', 'ONDOUSDT', 'ONEUSDT', 'ONTUSDT', 'OPUSDT', 'ORBSUSDT',
+    'ORDIUSDT', 'OXTUSDT', 'PENDLEUSDT', 'PEOPLEUSDT', 'PERPUSDT', 'PHBUSDT', 'PIXELUSDT', 'POLYXUSDT', 'PORTALUSDT', 'POWRUSDT', 'PYTHUSDT', 'QNTUSDT', 'QTUMUSDT'
+]
+
+# Time interval for fetching historical data
+time_interval = '1m'  # You can change this to '4h', '1d', etc.
