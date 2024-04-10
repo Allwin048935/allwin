8490d9462cbd6d793c13abb0ef4f3df71cde402f
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-04-10 12:32:21 +0530

    Update config2.py

--- config2.py
+++ config2.py
@@ -1,1 +1,19 @@
 
+# config.py
+
+BINANCE_API_KEY = 'veiqd07BuRmMlxy3eeRLLKNyDnFrIphTcqgSM7XTRUCzQWTyqxK4sPtfVZioaVHi'
+BINANCE_API_SECRET = 'N4myBDNkkD203gxxpo8NLQgdZxdvzm7N7PpBjBU0DiTtctTLaFQEVev51N4P5R5g'
+
+# Telegram API keys
+#TELEGRAM_BOT_TOKEN = ''
+#TELEGRAM_CHAT_ID = ''
+
+
+# List of futures trading pairs (symbols)
+symbols = [
+    '1INCHUSDT', '1000SATSUSDT', '1000BONKUSDT', '1000FLOKIUSDT', '1000XECUSDT', '1000LUNCUSDT', '1000PEPEUSDT', '1000SHIBUSDT', 'BADGERUSDT', 'BALUSDT', 'BANDUSDT', 'BAKEUSDT', 'BATUSDT', 'BEAMXUSDT', 'BELUSDT', 'BCHUSDT', 'BICOUSDT', 'BIGTIMEUSDT',
+    'BLURUSDT', 'BLZUSDT', 'BNBUSDT', 'BNTUSDT', 'BOMEUSDT', 'BONDUSDT', 'BSVUSDT', 'BTCUSDT'
+]
+
+# Time interval for fetching historical data
+time_interval = '1m'  # You can change this to '4h', '1d', etc.
