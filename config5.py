c2d15531df20bcde387294727312d6e40b2bc849
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-04-10 12:35:21 +0530

    Update config5.py

--- config5.py
+++ config5.py
@@ -1,1 +1,20 @@
 
+# config.py
+
+BINANCE_API_KEY = 'veiqd07BuRmMlxy3eeRLLKNyDnFrIphTcqgSM7XTRUCzQWTyqxK4sPtfVZioaVHi'
+BINANCE_API_SECRET = 'N4myBDNkkD203gxxpo8NLQgdZxdvzm7N7PpBjBU0DiTtctTLaFQEVev51N4P5R5g'
+
+# Telegram API keys
+#TELEGRAM_BOT_TOKEN = ''
+#TELEGRAM_CHAT_ID = ''
+
+
+# List of futures trading pairs (symbols)
+symbols = [
+    'JASMYUSDT', 'JOEUSDT', 'JTOUSDT', 'JUPUSDT', 'KASUSDT', 'KAVAUSDT', 'KEYUSDT', 'KNCUSDT',
+    'KLAYUSDT', 'KNCUSDT', 'KSMUSDT', 'LDOUSDT', 'LEVERUSDT', 'LINKUSDT', 'LINAUSDT', 'LITUSDT', 'LOOMUSDT', 'LPTUSDT', 'LQTYUSDT', 'LRCUSDT', 'LSKUSDT', 'LTCUSDT', 'LUNA2USDT', 'MAGICUSDT', 'MANTAUSDT',
+    'MANAUSDT', 'MASKUSDT', 'MATICUSDT', 'MAVIAUSDT', 'MAVUSDT', 'MBLUSDT', 'MDTUSDT', 'MEMEUSDT', 'METISUSDT', 'MINAUSDT', 'MKRUSDT', 'MOVRUSDT', 'MTLUSDT', 'MYROUSDT'
+]
+
+# Time interval for fetching historical data
+time_interval = '1m'  # You can change this to '4h', '1d', etc.
